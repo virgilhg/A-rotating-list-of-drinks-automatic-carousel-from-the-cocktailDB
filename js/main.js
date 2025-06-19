aadf88a41734acb4c279bf652deb920deba4cdf3
Virgil_HD <120189340+virgilhg@users.noreply.github.com>
2025-06-19 16:00:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,16 +6,40 @@ function getDrink(){
   fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+ drink )
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data.drinks[0])
-        document.querySelector('h2').innerText=data.drinks[0].strDrink
-        document.querySelector('img').src=data.drinks[0].strDrinkThumb
-        document.querySelector('h3').innerText=data.drinks[0].strInstructions
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+      const drinks = data.drinks;
+      if (!drinks) {
+        document.querySelector('h2').innerText = 'No drinks found.';
+        document.querySelector('img').src = '';
+        document.querySelector('h3').innerText = '';
+        return;
+      }
+
+      let current = 0;                          // index of the drink we are showing
+      let timer;                                // will hold the setInterval id
+
+      // helper: put one drink’s info in the page
+      function showDrink(i) {
+        const d = drinks[i];
+        document.querySelector('h2').innerText = d.strDrink;
+        document.querySelector('img').src       = d.strDrinkThumb;
+        document.querySelector('h3').innerText = d.strInstructions;
+      }
+
+      // show the first drink immediately
+      showDrink(current);
+
+      // stop a previous interval if the user searches again
+      if (timer) clearInterval(timer);
+
+      // every 3 seconds, advance to the next drink
+      timer = setInterval(function () {
+        current = (current + 1) % drinks.length; // loop back to 0 at the end
+        showDrink(current);
+      }, 3000);
+    });
 
     }
+
 
 
 
